c0ac94d9953d179d43bf22a3594ffb8bff50e4a0
AnrDaemon <anrdaemon@yandex.ru>
2018-08-27 19:49:55 +0300

    Made fancy error pages generated from a list

---
+++ etc/nginx/extras/fancy_error_pages.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+_tpl='
+error_page %u /-error-%u;
+location = /-error-%u {
+    internal;
+    default_type text/html;
+    return %u "<!DOCTYPE html>
+<html><head>
+<title>%u %s</title>
+</head><body><h1>%s</h1>
+<p>%s</p>
+<hr/>
+<address>nginx/$nginx_version server at $host port $server_port</address>
+</body></html>";
+}
+'
+
+echo "# Fancy error pages for nginx webserver"
+while IFS=: read -r _code _name _desc _ref; do
+    [ "$_code" ] && printf "$_tpl" $_code $_code $_code $_code $_code "$_name" "$_name" "${_desc:-$_name}"
+done < "fancy_error_pages.list"
