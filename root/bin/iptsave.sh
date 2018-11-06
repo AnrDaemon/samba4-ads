86e116a94b65915f454c03654ea9ea2ebfb38a8b
AnrDaemon <anrdaemon@yandex.ru>
2018-11-06 20:57:02 +0300

    Fixed iptsave.sh and .environment formats.

--- root/bin/iptsave.sh
+++ root/bin/iptsave.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
+#!/bin/bash
 cp --backup=numbered -aT -- /etc/network/if-up.d/iptables-rules /var/backups/iptables-rules
 echo "#!/sbin/iptables-restore" > /etc/network/if-up.d/iptables-rules
-/opt/scripts/iptables-save-ordered -r >> /etc/network/if-up.d/iptables-rules
+iptables-save-ordered -r >> /etc/network/if-up.d/iptables-rules
 chmod +x /etc/network/if-up.d/iptables-rules
