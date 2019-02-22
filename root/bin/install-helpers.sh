673832f5a150818ea2f1904110cc3f81e7fa3e34
AnrDaemon <anrdaemon@yandex.ru>
2019-02-22 22:44:25 +0300

    Cleaned up helpers installation script.

--- root/bin/install-helpers.sh
+++ root/bin/install-helpers.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+
 _helpers="
-lxc1:/etc/bash_completion.d/
+histpurge.php:/usr/local/bin/histpurge
+iptables-save-ordered.sh:/usr/local/sbin/iptables-save-ordered
 macvlan.sh:/etc/network/if-pre-up.d/macvlan
 macvlan.sh:/etc/network/if-post-down.d/macvlan
 ngdissite:/usr/local/sbin/
@@ -12,16 +15,16 @@ do
   [ "$helper" ] && {
     echo "$helper" | {
       IFS=: read -r helper dest
-      [ "$helper" -a "$dest" -a -f "/opt/scripts/$helper" ] || return
-      printf "Installing %s to '%s' - " "$helper" "$dest"
-      chmod +x "/opt/scripts/$helper"
-      if [ -d "$dest" ]; then
-        [ -L "$dest/$helper" -o ! -e "$dest/$helper" ] && ln -vfst "$dest" "/opt/scripts/$helper"
-      elif [ -L "$dest" -o ! -e "$dest" ]; then
-        ln -vfs "/opt/scripts/$helper" "$dest"
-      else
-        echo "already installed or blocked by existing file."
-        return
+      if [ "$helper" -a "$dest" -a -f "/opt/scripts/$helper" ]; then
+        printf "Installing %s: " "$helper"
+        chmod +x "/opt/scripts/$helper"
+        if [ -d "$dest" ]; then
+          [ -L "$dest/$helper" -o ! -e "$dest/$helper" ] && ln -vfst "$dest" "/opt/scripts/$helper"
+        elif [ -L "$dest" -o ! -e "$dest" ]; then
+          ln -vfs "/opt/scripts/$helper" "$dest"
+        else
+          echo "already installed or blocked by existing file."
+        fi
       fi
     }
   }
