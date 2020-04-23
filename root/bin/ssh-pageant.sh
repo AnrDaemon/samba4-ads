9df013eb1846992bb6591d9115a951c757f3e117
AnrDaemon <anrdaemon@yandex.ru>
2020-04-23 21:50:21 +0300

    Backported ssh-pageant changes from active script

--- root/bin/ssh-pageant.sh
+++ root/bin/ssh-pageant.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-test -r "$HOME/.ssh_agent" && {
-  . "$HOME/.ssh_agent"
+_agent="$HOME/.ssh/agent"
+test -f "$_agent" && {
+  . "$_agent"
   if kill -0 "$SSH_PAGEANT_PID" 2> /dev/null; then
-    if test -r $SSH_AUTH_SOCK; then
+    if test -S $SSH_AUTH_SOCK; then
       # Agent is alive, try to restart.
       # Fail if restart fails. (I.e. if agent is running elevated for some reason.)
-      ssh-pageant.exe -qk || exit 1
+      /usr/bin/ssh-pageant -qk || exit 1
     else
       # Socket not readable, we aren't running as a different user, are we?
       # Assume already dead agent.
@@ -15,14 +16,14 @@ test -r "$HOME/.ssh_agent" && {
   fi
   # Reap dead agent's socket
   rm "$SSH_AUTH_SOCK" 2> /dev/null
-  rm "$HOME/.ssh_agent"
+  rm "$_agent"
 }
 
-eval set -- $(getopt -o 'k' -- "$@")
+eval set -- $( getopt --shell=sh -o 'k' -- "$@" )
 test "$1" = "-k" && exit
 
-socket="$(mktemp -u /var/run/ssh-XXXXXXXX)"
-eval $(ssh-pageant.exe -qsa "$socket" | tee "$HOME/.ssh_agent")
+socket="$( mktemp -u /var/run/ssh-XXXXXXXX )"
+eval $( cygdrop -- /usr/bin/ssh-pageant -qsa "$socket" | tee "$_agent" )
 
 # Remove empty settings file (agent failed to start).
-test -s "$HOME/.ssh_agent" || rm "$HOME/.ssh_agent"
+test -s "$_agent" || rm "$_agent"
